d762c44ccb73fb9d367a9eb498772f2efef641eb
Jonathan Scholbach <j.scholbach@posteo.de>
2022-02-09 08:58:53 +0100

    add tests for Union defined by pipe operator

--- tests/test_types/test_union.py
+++ tests/test_types/test_union.py
@@ -1,3 +1,5 @@
+import sys
+
 from unittest import TestCase
 from typing import Literal, Union, TypeVar
 
@@ -33,6 +35,16 @@ class TestUnion(TestCase):
         primitive_union("")
         primitive_union(1)
 
+    def test_primitive_union_with_pipe_operator(self):
+        with self.assertRaises(TypeError):
+            primitive_union()
+
+        with self.assertRaises(RuntimeTypingError):
+            primitive_union(1.0)
+
+        primitive_union("")
+        primitive_union(1)
+
     def test_typed_union(self):
         union_with_type_var(1)
         union_with_type_var([1])
@@ -47,3 +59,54 @@ class TestUnion(TestCase):
 
         with self.assertRaises(RuntimeTypingError):
             complex_union(2.0)
+
+
+if sys.version_info >= (3, 10):
+
+    @typed
+    def primitive_union_with_pipe_operator(a: str | int):
+        pass
+
+    @typed
+    def union_with_type_var(a: T | str) -> T | str:
+        return a
+
+    @typed
+    def complex_union_with_pipe_operator(a: str | int | Literal[1.0]):
+        pass
+
+    class TestUnionWithPipeOperator(TestCase):
+        def test_primitive_union_with_pipe_operator(self):
+            with self.assertRaises(TypeError):
+                primitive_union_with_pipe_operator()
+
+            with self.assertRaises(RuntimeTypingError):
+                primitive_union_with_pipe_operator(1.0)
+
+            primitive_union_with_pipe_operator("")
+            primitive_union_with_pipe_operator(1)
+
+        def test_primitive_union_with_pipe_operator_with_pipe_operator(self):
+            with self.assertRaises(TypeError):
+                primitive_union_with_pipe_operator()
+
+            with self.assertRaises(RuntimeTypingError):
+                primitive_union_with_pipe_operator(1.0)
+
+            primitive_union_with_pipe_operator("")
+            primitive_union_with_pipe_operator(1)
+
+        def test_typed_union_with_pipe_operator(self):
+            union_with_type_var(1)
+            union_with_type_var([1])
+
+        def test_complex_union_with_pipe_operator(self):
+            complex_union_with_pipe_operator(1.0)
+            complex_union_with_pipe_operator(1)
+            complex_union_with_pipe_operator(1.0)
+
+            with self.assertRaises(RuntimeTypingError):
+                complex_union_with_pipe_operator([1, 2])
+
+            with self.assertRaises(RuntimeTypingError):
+                complex_union_with_pipe_operator(2.0)
